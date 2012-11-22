fa6fc40008e7d56cb4af905cf91dac533a2a6e7b
Roman Cheplyaka <roma@ro-che.info>
2012-11-22 21:13:19 +0200

    test: getInstalledPackages

--- test/test.hs
+++ test/test.hs
@@ -4,11 +4,13 @@ import Data.Version
 
 main = defaultMain testTool
   where
+  ldb = locateDB (error "GlobalDB") (error "LocalDB")
+
   testTool = HSTool
     { toolName = "myTool"
     , toolVersion = Version [3,1,4] []
-    , toolGetInstalledPkgs = \_ -> return []
+    , toolGetInstalledPkgs = readDB . ldb
     , toolCompile = \dir args -> print (dir, args)
     , toolInstallLib = \a b c d e -> print (a,b,c,d,e)
-    , toolRegister = register . locateDB (error "GlobalDB") (error "LocalDB")
+    , toolRegister = register . ldb
     }
