5850ee9788fceb9ecbd1aa9757d55074a229283f
Roman Cheplyaka <roma@ro-che.info>
2013-05-08 23:07:51 +0300

    Distribution.HaskellSuite

---
+++ Distribution/HaskellSuite.hs
@@ -0,0 +1,8 @@
+module Distribution.HaskellSuite
+  ( module Distribution.HaskellSuite.Packages
+  , module Distribution.HaskellSuite.Modules
+  )
+  where
+
+import Distribution.HaskellSuite.Packages
+import Distribution.HaskellSuite.Modules
