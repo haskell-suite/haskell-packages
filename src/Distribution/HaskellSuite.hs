40038ee6577e7061587f4b6b3d1ba584dafb51db
Roman Cheplyaka <roma@ro-che.info>
2013-05-16 13:15:38 +0300

    Docs

--- src/Distribution/HaskellSuite.hs
+++ src/Distribution/HaskellSuite.hs
@@ -1,3 +1,11 @@
+-- | This module re-exports all you need in order to /read/ package
+-- databases and module info files created by compilers that use
+-- haskell-packages.
+--
+-- If you are writing a compiler, i.e. a program that creates or writes
+-- package databases or module info files — then take a look at
+-- "Distribution.HaskellSuite.Compiler". It provides command-line
+-- options handling and Cabal integration.
 module Distribution.HaskellSuite
   ( module Distribution.HaskellSuite.Packages
   , module Distribution.HaskellSuite.Modules
