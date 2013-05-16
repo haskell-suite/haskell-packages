0151a915c015c5172ca611ebfd195ff31bdd4ccd
Roman Cheplyaka <roma@ro-che.info>
2013-05-16 12:37:59 +0300

    Docs

--- src/Distribution/HaskellSuite/Compiler.hs
+++ src/Distribution/HaskellSuite/Compiler.hs
@@ -4,15 +4,19 @@
 -- >import qualified Distribution.HaskellSuite.Compiler as Compiler
 module Distribution.HaskellSuite.Compiler
   (
-    -- * Compiler description
+  -- * Compiler description
     Is(..)
   , CompileFn
 
-    -- * Simple compiler
+  -- * Simple compiler
   , Simple
   , simple
 
-    -- * Command line
+  -- * Command line
+  -- | Compiler's entry point.
+  --
+  -- It parses command line options (that are typically passed by Cabal) and
+  -- invokes the appropriate compiler's methods.
   , main
   )
   where
